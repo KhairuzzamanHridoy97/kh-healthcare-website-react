f6ddf8fd5496cecceaabc035dba56ea36e151940
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2021-10-22 18:39:23 +0600

    add extra section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,14 +5,25 @@ import { Card, Container, ProgressBar } from 'react-bootstrap';
 const About = () => {
     return (
         <div>
+            <div>
+            <h1 className='text-primary m-3'> Welcome to KH Medicare Hospital </h1>
+            <h2>We Provide the Best Medical Service to our Customer with trust</h2>
+            <h5><i>
+                We are the pioneer organization for general medical treatment and in cancer diagnosis, treatment, and management in the private sector in Bangladesh. It’s provides the full range of radiation therapy, diagnostic facility, chemotherapy and surgery with consultant, pathologist, surgeon, medical Oncologist, radiation oncologist & other physicians.
+
+                The diagnostic service is running by highly qualified internationally reputed pathologist having in the experience of more than 32 years and other qualified personal with full range of diagnostic facility. The radiology department has also well qualified clinician and facility for digital x-ray, ultrasonography and CT scan.
+
+                Using state- of- the-art equipment, our specialists are able to provide the best possible radiation therapy at our centers. </i>
+            </h5>
+            </div>
              <div>
                  <h1 className="text-primary m-3">About KH Medicare Hospital</h1>
-            <h3 className='m-3'><i>
-                KH Medicare Hospital has been in existence since 1989. It is approved by concerned authorities like Directorate General of Health Services, Dhaka City Corporation, Ministry of Environment, Ministry of Health and Family Welfare, Bangladesh Atomic Energy Commission.
+                <h5 className='m-3'><i>
+                     KH Medicare Hospital has been in existence since 1989. It is approved by concerned authorities like Directorate General of Health Services, Dhaka City Corporation, Ministry of Environment, Ministry of Health and Family Welfare, Bangladesh Atomic Energy Commission.
 
-                KH Medicare Hospital prides itself on its commitment to the patients and is constantlystriving to improve diagnostic performance and heightened satisfaction of patients and physicians delight.</i>
-            </h3><br /> <br />
-        </div>
+                     KH Medicare Hospital prides itself on its commitment to the patients and is constantlystriving to improve diagnostic performance and heightened satisfaction of patients and physicians delight.</i>
+                </h5><br /> <br />
+            </div>
         <div>
             <h2 className="text-primary m-3">Our Objective</h2>
             <h5 className='p-2 mb-5'><i>
@@ -25,16 +36,10 @@ const About = () => {
 
         <div>
             
-        <div>
-            <h3 className='text-primary m-5'>Our Progress (2017-2020)</h3>
-            <Container className=''>
-            <ProgressBar  variant="success" now={40} /> <br />
-            <ProgressBar striped variant="info" now={20} /> <br />
-            <ProgressBar striped variant="warning" now={60} /> <br />
-            <ProgressBar striped variant="danger" now={80} />
+      
 
-            </Container>
-        </div>
+
+        
         </div>
 
         </div>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, ProgressBar } from 'react-bootstrap';
 import Banner from '../Banner/Banner';
 import Doctors from '../Doctors/Doctors';
 
@@ -8,6 +9,17 @@ const Home = () => {
             <Banner></Banner>
             <h1 className='text-primary m-5'>Our Doctors</h1>
             <Doctors></Doctors>
+            <div>
+            <h3 className='text-primary m-5'>Our Progress (2017-2020)</h3>
+            <Container className=''>
+            <ProgressBar  variant="success" now={40} /> <br />
+            <ProgressBar striped variant="info" now={20} /> <br />
+            <ProgressBar striped variant="warning" now={60} /> <br />
+            <ProgressBar striped variant="danger" now={80} />
+
+            </Container>
+        </div>
+
         </div>
     );
 };
